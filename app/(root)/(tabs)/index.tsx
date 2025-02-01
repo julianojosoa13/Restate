2256161198c67de2b1663d246236f0bbdfeb1262
julianojosoa13 <josoajuliano@gmail.com>
2025-02-01 11:30:15 +0300

    Fixing some more nativewind configuration mistakes

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -10,6 +10,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
+      <Text className="font-bold text-lg my-10">Welcome to ReState</Text>
       <Link href={"/signIn"}>Sign In</Link>
       <Link href={"/Explore"}>Explore</Link>
       <Link href={"/Profile"}>Profile</Link>
