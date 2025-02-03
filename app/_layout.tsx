fabc1087ae2b2b8f017887b977bb2134bd0948e0
julianojosoa13 <josoajuliano@gmail.com>
2025-02-03 04:27:19 +0300

    Sign in screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -21,5 +21,5 @@ export default function RootLayout() {
   }, [fontsLoaded]);
 
   if (!fontsLoaded) return null;
-  return <Stack />;
+  return <Stack screenOptions={{ headerShown: false }} />;
 }
--- app/signIn.tsx
+++ app/signIn.tsx
@@ -1,18 +1,30 @@
+import images from "@/constants/images";
 import React, { FC } from "react";
-import { SafeAreaView, StyleSheet, Text } from "react-native";
+import { Image, SafeAreaView, ScrollView, Text, View } from "react-native";
 
 interface Props {}
 
 const signIn: FC<Props> = (props) => {
   return (
-    <SafeAreaView style={styles.container}>
-      <Text>Sign In</Text>
+    <SafeAreaView className="bg-white h-full">
+      <ScrollView contentContainerClassName="h-full">
+        <Image
+          source={images.onboarding}
+          className="w-full h-4/6"
+          resizeMode="contain"
+        />
+        <View className="px-10">
+          <Text className="text-base text-center uppercase font-rubik text-black-200">
+            Welcome to ReState
+          </Text>
+          <Text className="text-3xl font-rubik-bold text-black-300 text-center mt-2">
+            Let's get you close to {"\n"}
+            <Text className="text-primary-300">Your ideal home</Text>
+          </Text>
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {},
-});
 
 export default signIn;
